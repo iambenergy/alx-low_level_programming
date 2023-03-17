e6b7576f5c1c688c2e2d42478acff712a51978e0
Benjamin Okoro <be_okoro@yahoo.com>
2023-03-17 06:25:13 -0700

    Alphabets in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry point to the program
+ *
+ * Return: Return 0 successfully
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i <= 'z'; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
