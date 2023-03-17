e8dd014c85c16915c43fe95852d908d87cacc963
Benjamin Okoro <be_okoro@yahoo.com>
2023-03-17 03:57:50 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Main entry point
+ *
+ * Return: Return 0 Success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
